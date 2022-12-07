65e032f726b9c1b7f07b975ab1e7884b8e9b673c
priyaaranjith <ranjithpriya307@gmail.com>
2022-12-07 11:00:00 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddemployeeComponent } from './addemployee/addemployee.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const myRoute : Routes = [
+  {
+    path:"",
+    component:AddemployeeComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +23,8 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
